57fdc98dec069c6b6cbcfbdd77eb37a3e2e6e28b
Gregory Estrade <gregory.estrade@gmail.com>
2015-10-04 11:37:57 +0200

    First run of verilated Z80 SoC.

---
+++ main.cpp
@@ -0,0 +1,122 @@
+#define DPI_DLLISPEC
+#define DPI_DLLESPEC
+
+#include "verilated.h"
+#include "svdpi.h"
+
+#include "Vz88.h"
+
+#include <ctime>
+
+#if VM_TRACE
+#include "verilated_vcd_c.h"
+#endif
+
+// Number of simulation steps
+#define NUM_STEPS    ((vluint64_t)205000)
+// Half period (in ps) of a 33.333 MHz clock
+#define STEP_PS      ((vluint64_t)15000)
+
+// Simulation steps (global)
+vluint64_t tb_sstep;
+vluint64_t tb_time;
+
+int main(int argc, char **argv, char **env)
+{
+    // Trace index
+    int trc_idx = 0;
+    // File name generation
+    char file_name[256];
+    // Simulation duration
+    time_t beg, end;
+    double secs;
+
+    beg = time(0);
+
+    Verilated::commandArgs(argc, argv);
+    // Init top verilog instance
+    Vz88* top = new Vz88;
+
+#if VM_TRACE
+    // Init VCD trace dump
+    Verilated::traceEverOn(true);
+    VerilatedVcdC* tfp = new VerilatedVcdC;
+    top->trace (tfp, 99);
+    tfp->spTrace()->set_time_resolution ("1 ps");
+    sprintf(file_name, "z88_%04d.vcd", trc_idx++);
+    tfp->open (file_name);
+#endif
+
+    // Initialize simulation inputs
+    top->reset_n = 0;
+    top->clk = 1;
+
+    top->ram_di = 0;
+    top->rom_di = 0;
+
+    tb_sstep = 0;  // Simulation steps (64 bits)
+    tb_time = 0;  // Simulation time in ps (64 bits)
+
+    // Load the ROM file
+    vluint8_t ROM[(1<<22)];
+    FILE *f = fopen("Z88UK400.rom","rb");
+    size_t size = fread(ROM, 1, 131072, f);
+    fclose(f);
+    printf("Loaded %ld bytes from ROM.\n", size);
+
+    // Run simulation for NUM_CYCLES clock periods
+    while (tb_sstep < NUM_STEPS)
+    {
+        // Reset ON during 12 cycles
+        top->reset_n = (tb_sstep < (vluint64_t)24) ? 0 : 1;
+        // Toggle clock
+        top->clk = top->clk ^ 1;
+        // Evaluate verilated model
+        top->eval();
+
+        // Simulate ROM behaviour
+        if (!top->rom_oe_n && !top->rom_ce_n) {
+          top->rom_di = ROM[top->rom_a & (131072-1)];
+        } else {
+          top->rom_di = 0xFF;
+        }
+
+#if VM_TRACE
+        // Dump signals into VCD file
+        if (tfp)
+        {
+            if (0)
+            {
+                // New VCD file
+                tfp->close();
+                sprintf(file_name, "z88_%04d.vcd", trc_idx++);
+                tfp->open(file_name);
+            }
+            tfp->dump(tb_time);
+        }
+#endif
+
+        // Next simulation step
+        tb_time += STEP_PS;
+        tb_sstep++;
+
+        if ((tb_sstep & 4095) == 0)
+        {
+            printf("\r%lld us", tb_time / 1000000 );
+            fflush(stdout);
+        }
+
+        if (Verilated::gotFinish()) break;
+    }
+    top->final();
+
+#if VM_TRACE
+    if (tfp) tfp->close();
+#endif
+
+    end = time(0);
+    secs = difftime(end, beg);
+    printf("\n\nSeconds elapsed : %f\n", secs);
+
+    exit(0);
+}
