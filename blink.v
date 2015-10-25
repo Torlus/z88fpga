7a912ab9c1f764bb189764128d880256b7a80c53
Thierry Peycru <elpekos@gmail.com>
2015-10-25 09:24:34 +0100

    Blink gate array draft.

---
+++ blink.v
@@ -0,0 +1,166 @@
+module blink (
+  // Outputs
+  rout_n, cdo, wrb_n, ipce_n, irce_n, se1_n, se2_n, se3_n, ma, pm1, rout_n,
+  intb_n, nmib_n, roe_n,
+  // Inputs
+  ca, crd_n, cdi, mck, sck, rin_n, hlt_n, mrq_n, ior_n, cm1_n, kbmat
+  );
+
+// Clocks
+input           mck;      // 9.83MHz Master Clock
+input           sck;      // 25.6KHz Standby Clock
+output          pm1;      // Z80 clock driven by blink
+
+// Reset
+input           rin_n;    // Reset button
+output          rout_n;   // Z80 reset
+
+// Logical memory (16 bits Z80 address bus)
+input   [15:0]  ca;       // Z80 address bus
+
+// Physical memory (22 bits address bus)
+output  [21:0]  ma;       // slot 0 (internal) to slot 3
+
+// Data bus
+input   [7:0]   cdi;
+output  [7:0]   cdo;
+
+// Control bus
+input           hlt_n;    // HALT Coma/Standby command
+input           crd_n;    // RD from Z80
+input           cm1_n;    // M1 (=WR or =RFSH) from Z80
+input           mrq_n;    // MREQ
+input           ior_n;    // ior_nQ
+output          ipce_n;   // Internal PROM Chip Enable
+output          irce_n;   // Internal RAM Chip Enable
+output          se1_n;    // Slot 1 select (CE)
+output          se2_n;    // Slot 2 select (CE)
+output          se3_n;    // Slot 3 select (CE)
+// output          poe_n;    // OE refreshed during coma for dynamic RAM (all slots)
+output          roe_n;    // OE for SRAM or ROM in Slot 0, 1, 2
+// output          eoe_n;    // OE for EPROM programmer in Slot 3
+output          wrb_n;    // WE (1=RD ; 0=WR)
+output          nmib_n;   // NMI
+output          intb_n;   // INT
+
+// Cards, EPROM programmer, Battery and Speaker
+// input           btl_n;    // Batt Low (<4.2V)
+// input           flp;      // Flap open of slot connector
+// input           sns_n;    // Sens Line for card insertion/removal and Batt <3.2V (induce NMI>>HLT)
+// output          pgmb_n;   // PGM
+// output          vpon;     // VPP
+// output          spkr;     // Speaker
+
+// LCD
+// output   [3:0]  ldb;      // line data 0-3
+// output          xscl;     // 300ns nibble pulse
+// output          lp;       // 156us line pulse
+// output          fr;       // 10ms frame reverse
+
+// Serial port
+// input           rxd;      // RX line
+// input           cts;      // CTS handshaking
+// input           dcd;      // DCD control
+// output          txd;      // TX line
+// output          rts;      // RTS handshaking
+
+// Keyboard
+input           kbmat;
+
+// Reset
+assign rout_n = rin_n;
+
+// Clocks
+assign pm1 = mck;
+
+// General
+reg     [7:0]   r_cdo;
+
+// Bank switching
+reg     [7:0]   com;      // IO $B0
+`define RAMS 2
+reg     [7:0]   sr0;
+reg     [7:0]   sr1;
+reg     [7:0]   sr2;
+reg     [7:0]   sr3;
+
+assign ma =
+  (ca[15:14] == 2'b11) ? { sr3, ca[13:0] }                // C000-FFFF
+  :  (ca[15:14] == 2'b10) ? { sr2, ca[13:0] }             // 8000-BFFF
+  :  (ca[15:14] == 2'b01) ? { sr1, ca[13:0] }             // 4000-7FFF
+  :  (ca[15:13] == 3'b001) ? { sr0, 1'b1, ca[12:0] }      // 2000-3FFF
+  :  (ca[15:13] == 3'b000) ?                              // 0000-1FFF
+    (com[2] == 1'b0) ? { 8'b00000000, 1'b0, ca[12:0] }    // Bank $00 !RAMS
+    : { 8'b00010000, 1'b0, ca[12:0] }                     // Bank $20 RAMS
+  : 22'b11_1111_1111_1111_1111_1111;
+
+assign ipce_n =
+  (ma[21:19] == 3'b000 & !mrq_n) ? 1'b0 : 1'b1;
+
+assign irce_n =
+  (ma[21:19] == 3'b001 & !mrq_n) ? 1'b0 : 1'b1;
+
+assign wrb_n = (!mrq_n & crd_n) ? 1'b0 : 1'b1;
+assign roe_n = (!mrq_n & !crd_n) ? 1'b0 : 1'b1;
+assign cdo = (!ior_n) ? r_cdo : cdi;
+
+always @(posedge mck)
+begin
+  if (rin_n == 1'b0) begin
+    com <= 8'b00000000;
+  end else if (mck == 1'b1) begin
+    if (!ior_n & crd_n) begin
+      // IO register write
+      case(ca[7:0])
+        8'h70: pb0 <= {ca[12:8], cdi};
+        8'h71: pb1 <= {ca[9:8], cdi};
+        8'h72: pb2 <= {ca[8], cdi};
+        8'h73: pb3 <= {ca[10:8], cdi};
+        8'h74: sbr <= {ca[10:8], cdi};
+        8'hB0: com <= cdi;
+        8'hD0: sr0 <= cdi;
+        8'hD1: sr1 <= cdi;
+        8'hD2: sr2 <= cdi;
+        8'hD3: sr3 <= cdi;
+        default: ;
+      endcase
+    end else if (!ior_n & !crd_n) begin
+      // IO register read
+      case(ca[7:0])
+        8'hB2: r_cdo <= kbd;
+        8'hD0: r_cdo <= sr0;
+        8'hD1: r_cdo <= sr1;
+        8'hD2: r_cdo <= sr2;
+        8'hD3: r_cdo <= sr3;
+        default: ;
+      endcase
+    end
+  end
+end
+
+// Keyboard
+reg     [63:0]  kbmat;
+wire    [7:0]   kbcol[0:7];
+wire    [7:0]   kbd;
+
+assign kbcol[0] = ca[ 8] ? kbmat[ 7: 0] : 8'b00000000;
+assign kbcol[1] = ca[ 9] ? kbmat[15: 8] : 8'b00000000;
+assign kbcol[2] = ca[10] ? kbmat[23:16] : 8'b00000000;
+assign kbcol[3] = ca[11] ? kbmat[31:24] : 8'b00000000;
+assign kbcol[4] = ca[12] ? kbmat[39:32] : 8'b00000000;
+assign kbcol[5] = ca[13] ? kbmat[47:40] : 8'b00000000;
+assign kbcol[6] = ca[14] ? kbmat[55:48] : 8'b00000000;
+assign kbcol[7] = ca[15] ? kbmat[63:56] : 8'b00000000;
+
+assign kbd = kbcol[0] | kbcol[1] | kbcol[2] | kbcol[3]
+  & kbcol[4] | kbcol[5] | kbcol[6] | kbcol[7];
+
+// Display
+reg     [12:0]  pb0;  // Lores0 (RAM, 64 char, 512B)
+reg     [9:0]   pb1;  // Lores1 (ROM, 448 char, 3.5K)
+reg     [8:0]   pb2;  // Hires0 (RAM, 768 char, 6K)
+reg     [10:0]  pb3;  // Hires1 (ROM, 256 char, 2K)
+reg     [10:0]  sbr;  // Screen Base File (RAM, 128 attr*8, 2K)
+
+
+endmodule
--- z88.v
+++ z88.v
@@ -7,7 +7,7 @@ module z88 (
   clk, reset_n,
   ps2clk, ps2dat,
   ram_di,
-  rom_di
+  rom_do
 );
 
 // Clock and Reset
@@ -18,175 +18,124 @@ input           reset_n;
 input           ps2clk;
 input           ps2dat;
 
-// RAM
+// Internal RAM
 output  [18:0]  ram_a;
-output  [7:0]   ram_do;
-input   [7:0]   ram_di;
+output  [7:0]   ram_di;
+input   [7:0]   ram_do;
 output          ram_ce_n;
 output          ram_oe_n;
 output          ram_we_n;
 
-// ROM
+// Internal ROM
 output  [18:0]  rom_a;
-input   [7:0]   rom_di;
+input   [7:0]   rom_do;
 output          rom_ce_n;
 output          rom_oe_n;
 
-// Z80
-wire            z80_m1_n;
-wire            z80_mreq_n;
-wire            z80_iorq_n;
-wire            z80_rd_n;
-wire            z80_wr_n;
-wire            z80_halt_n;
-wire            z80_busak_n;
-wire    [15:0]  z80_a;
-wire    [7:0]   z80_do;
-wire            z80_reset_n;
-wire            z80_clk;
-wire            z80_wait_n;
-wire            z80_int_n;
-wire            z80_nmi_n;
-wire            z80_busrq_n;
-wire    [7:0]   z80_di;
+// Z88 PCB glue
+wire            z88_mck;      // master clock
+wire            z88_sck;      // standby clock
+wire            z88_pm1;      // Z80 clock
+wire            z88_m1_n;
+wire            z88_mreq_n;
+wire            z88_iorq_n;
+wire            z88_rd_n;
+wire            z88_halt_n;
+wire            z88_reset_n;
+wire            z88_int_n;
+wire            z88_nmi_n;
+wire            z88_busrq_n;
+wire    [21:0]  z88_ma;
+wire    [15:0]  z88_ca;
+wire    [7:0]   z88_cdo;
+wire    [7:0]   z88_cdi;
+wire            z88_ipce_n;
+wire            z88_irce_n;
+wire            z88_se1_n;
+wire            z88_se2_n;
+wire            z88_se3_n;
+wire            z88_roe_n;
+wire            z88_we_n;
+wire            z88_rin_n;
+wire            z88_rout_n;
+wire    [63:0]  z88_kbmat;
 
-// Blink
-reg     [7:0]   com;    // IO $B0
-`define RAMS 2
-reg     [7:0]   sr0;
-reg     [7:0]   sr1;
-reg     [7:0]   sr2;
-reg     [7:0]   sr3;
-
-wire    [21:0]  bl_a;
-wire            bl_ipce;
-wire            bl_irce;
-
-reg     [7:0]   ioport_do;
-
-reg     [63:0]  kbmat;
-wire    [7:0]   kbcol[0:7];
-wire    [7:0]   kbd;
-
-assign kbcol[0] = z80_a[ 8] ? kbmat[ 7: 0] : 8'b00000000;
-assign kbcol[1] = z80_a[ 9] ? kbmat[15: 8] : 8'b00000000;
-assign kbcol[2] = z80_a[10] ? kbmat[23:16] : 8'b00000000;
-assign kbcol[3] = z80_a[11] ? kbmat[31:24] : 8'b00000000;
-assign kbcol[4] = z80_a[12] ? kbmat[39:32] : 8'b00000000;
-assign kbcol[5] = z80_a[13] ? kbmat[47:40] : 8'b00000000;
-assign kbcol[6] = z80_a[14] ? kbmat[55:48] : 8'b00000000;
-assign kbcol[7] = z80_a[15] ? kbmat[63:56] : 8'b00000000;
-
-assign kbd = kbcol[0] | kbcol[1] | kbcol[2] | kbcol[3]
-  & kbcol[4] | kbcol[5] | kbcol[6] | kbcol[7];
-
-reg     [12:0]  pb0;  // Lores0 (RAM, 64 char, 512B)
-reg     [9:0]   pb1;  // Lores1 (ROM, 448 char, 3.5K)
-reg     [8:0]   pb2;  // Hires0 (RAM, 768 char, 6K)
-reg     [10:0]  pb3;  // Hires1 (ROM, 256 char, 2K)
-reg     [10:0]  sbr;  // Screen Base File (RAM, 128 attr*8, 2K)
+// Clocks
+assign z88_mck = clk;
 
 // Z80 instance
 tv80s z80 (
-  .m1_n(z80_m1_n),
-  .mreq_n(z80_mreq_n),
-  .iorq_n(z80_iorq_n),
-  .rd_n(z80_rd_n),
-  .wr_n(z80_wr_n),
-  .rfsh_n(),
-  .halt_n(z80_halt_n),
-  .busak_n(z80_busak_n),
-  .A(z80_a),
-  .dout(z80_do),
-  .reset_n(z80_reset_n),
-  .clk(z80_clk),
-  .wait_n(z80_wait_n),
-  .int_n(z80_int_n),
-  .nmi_n(z80_nmi_n),
-  .busrq_n(z80_busrq_n),
-  .di(z80_di)
+  .m1_n(z88_m1_n),
+  .mreq_n(z88_mreq_n),
+  .iorq_n(z88_iorq_n),
+  .rd_n(z88_rd_n),
+  .wr_n(),                  // not wired
+  .rfsh_n(),                // not wired
+  .halt_n(z88_halt_n),
+  .busak_n(),               // not wired
+  .A(z88_ca),
+  .dout(z88_cdi),
+  .reset_n(z88_rout_n),
+  .clk(z88_pm1),
+  .wait_n(),                // not wired
+  .int_n(z88_int_n),
+  .nmi_n(z88_nmi_n),
+  .busrq_n(),               // not wired
+  .di(z88_cdo)
 );
+
+assign z88_int_n = 1'b1;
+assign z88_nmi_n = 1'b1;
+
+// Blink instance
+blink theblink (
+  .rout_n(z88_rout_n),
+  .rin_n(z88_reset_n),
+  .mck(z88_mck),
+  .sck(z88_sck),
+  .pm1(z88_pm1),
+  .cdi(z88_cdi),
+  .cdo(z88_cdo),
+  .ca(z88_ca),
+  .ma(z88_ma),
+  .hlt_n(z88_halt_n),
+  .nmib_n(z88_nmi_n),
+  .intb_n(z88_int_n),
+  .ior_n(z88_iorq_n),
+  .mrq_n(z88_mreq_n),
+  .cm1_n(z88_m1_n),
+  .crd_n(z88_rd_n),
+  .wrb_n(z88_we_n),
+  .roe_n(z88_roe_n),
+  .ipce_n(z88_ipce_n),
+  .irce_n(z88_irce_n),
+  .se1_n(z88_se1_n),
+  .se2_n(z88_se2_n),
+  .se3_n(z88_se3_n),
+  .kbmat(z88_kbmat)
+);
+
+// Internal RAM (Slot 0)
+assign ram_a = z88_ma[18:0];
+assign ram_do = z88_cdi;
+assign ram_di = z88_cdo;
+assign ram_we_n = z88_we_n;
+assign ram_oe_n = z88_roe_n;
+assign ram_ce_n = z88_irce_n;
+
+// Internal ROM (Slot 0)
+assign rom_a = z88_ma[18:0];
+assign rom_do = z88_cdi;
+assign rom_oe_n = z88_roe_n;
+assign rom_ce_n = z88_ipce_n;
 
 // PS/2 keyboard
 ps2 ps2kb (
-  .reset_n(reset_n),
+  .reset_n(z88_rout_n),
   .ps2clk(ps2clk),
   .ps2dat(ps2dat),
-  .kbmat_out(kbmat)
+  .kbmat_out(z88_kbmat)
 );
 
-assign z80_reset_n = reset_n;
-assign z80_clk = clk;
-assign z80_wait_n = 1'b1;
-assign z80_int_n = 1'b1;
-assign z80_nmi_n = 1'b1;
-assign z80_busrq_n = 1'b1;
-
-assign bl_a =
-  (z80_a[15:14] == 2'b11) ? { sr3, z80_a[13:0] }
-  :  (z80_a[15:14] == 2'b10) ? { sr2, z80_a[13:0] }
-  :  (z80_a[15:14] == 2'b01) ? { sr1, z80_a[13:0] }
-  :  (z80_a[15:13] == 3'b001) ? { sr0, 1'b1, z80_a[12:0] }
-  :  (z80_a[15:13] == 3'b000) ?
-    (com[2] == 1'b0) ? { 8'b00000000, 1'b0, z80_a[12:0] }
-    : { 8'b00010000, 1'b0, z80_a[12:0] }
-  : 22'b11_1111_1111_1111_1111_1111;
-
-assign bl_ipce =
-  (bl_a[21:19] == 3'b000) ? 1'b1 : 1'b0;
-
-assign bl_irce =
-  (bl_a[21:19] == 3'b001) ? 1'b1 : 1'b0;
-
-
-assign ram_a = bl_a[18:0];
-assign ram_do = z80_do;
-assign ram_we_n = (!z80_mreq_n & !z80_wr_n) ? 1'b0 : 1'b1;
-assign ram_oe_n = (!z80_mreq_n & !z80_rd_n) ? 1'b0 : 1'b1;
-assign ram_ce_n = (!z80_mreq_n & bl_irce) ? 1'b0 : 1'b1;
-
-assign rom_a = bl_a[18:0];
-assign rom_oe_n = (!z80_mreq_n & !z80_rd_n) ? 1'b0 : 1'b1;
-assign rom_ce_n = (!z80_mreq_n & bl_ipce) ? 1'b0 : 1'b1;
-
-assign z80_di =
-  !z80_iorq_n ? ioport_do
-  : bl_ipce ? rom_di
-  : bl_irce ? ram_di
-  : 8'b11111111;
-
-always @(posedge clk)
-begin
-  if (reset_n == 1'b0) begin
-    com <= 8'b00000000;
-  end else if (clk == 1'b1) begin
-    if (!z80_iorq_n & !z80_wr_n) begin
-      // IO register write
-      case(z80_a[7:0])
-        8'h70: pb0 <= {z80_a[12:8], z80_do};
-        8'h71: pb1 <= {z80_a[9:8], z80_do};
-        8'h72: pb2 <= {z80_a[8], z80_do};
-        8'h73: pb3 <= {z80_a[10:8], z80_do};
-        8'h74: sbr <= {z80_a[10:8], z80_do};
-        8'hB0: com <= z80_do;
-        8'hD0: sr0 <= z80_do;
-        8'hD1: sr1 <= z80_do;
-        8'hD2: sr2 <= z80_do;
-        8'hD3: sr3 <= z80_do;
-        default: ;
-      endcase
-    end else if (!z80_iorq_n & !z80_rd_n) begin
-      // IO register read
-      case(z80_a[7:0])
-        8'hB2: ioport_do <= kbd;
-        8'hD0: ioport_do <= sr0;
-        8'hD1: ioport_do <= sr1;
-        8'hD2: ioport_do <= sr2;
-        8'hD3: ioport_do <= sr3;
-        default: ;
-      endcase
-    end
-  end
-end
 
 endmodule
