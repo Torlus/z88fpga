25afb1837f6e7db727221ab411ccd9a2c37e0dcb
Thierry Peycru <elpekos@gmail.com>
2016-10-16 22:29:56 +0200

    Simplified LCD address generator.

--- z88_screen.v
+++ z88_screen.v
@@ -5,19 +5,19 @@ module z88_screen
     input           clk,          // Master clock (50 MHz)
     input           clk_ena,      // 12.5 MHz equivalent clock
     input           bus_ph,       // Bus phase (0 : LCD, 1 : Z80)
-    
+
     // Z80 bus
     input           z80_io_wr,    // Z80 I/O write
     input    [15:0] z80_addr,     // Z80 address bus
     input     [7:0] z80_wdata,    // Z80 data bus (write)
-    
+
     // LCD control
     input           new_fr_tgl,   // Fetch a new frame (toggle)
     output          lcd_rden,     // Reading LCD data
     output   [21:0] lcd_addr,     // 4 MB address space
     input           lcd_vld,      // 8-bit data is ready
     input     [7:0] lcd_rdata,    // 8-bit data from RAM/ROM
-    
+
     // LCD VRAM
     output          vram_we,      // Write enable
     output    [2:0] vram_data,    // Gray attribute + 2 pixels
@@ -29,7 +29,7 @@ module z88_screen
     // ========================================================================
     // LCD Registers Write
     // ========================================================================
-    
+
     // LCD screen registers
     reg [12:0] r_PB0;  // Lores0 (RAM, 64 char, 512B)
     reg  [9:0] r_PB1;  // Lores1 (ROM, 448 char, 3.5K)
@@ -38,7 +38,7 @@ module z88_screen
     reg [10:0] r_SBR;  // Screen Base File (RAM, 128 attr*8, 2K)
 
     always @(posedge rst or posedge clk) begin : LCD_REGS_WR
-    
+
         if (rst) begin
             r_PB0 <= 13'd0;
             r_PB1 <= 10'd0;
@@ -64,7 +64,7 @@ module z88_screen
     // ========================================================================
     // LCD matrix counters
     // ========================================================================
-    
+
     reg       r_lcd_run;   // LCD matrix scanning is running
     reg [2:0] r_lcd_cyc;   // LCD cycle (0 : SBA LSB, 1 : SBA MSB, 2 : )
     reg [6:0] r_col_ctr;   // 108 columns
@@ -72,11 +72,11 @@ module z88_screen
     reg       r_lcd_eol;   // End of LCD line
     reg       r_lcd_eof;   // End of LCD frame
     reg       r_blink;     // Blinking effect flag
-    
+
     always @(posedge rst or posedge clk) begin : LCD_MATRIX_CTR
         reg [5:0] v_fr_ctr;    // Frame counter (for blinking effect)
         reg [2:0] v_new_fr_cc; // New frame (from VGA controller)
-    
+
         if (rst) begin
             r_lcd_run   <= 1'b0;
             r_lcd_cyc   <= 3'b000;
@@ -100,7 +100,7 @@ module z88_screen
                         r_col_ctr <= r_col_ctr + 7'd1;
                     end
                 end
-        
+
                 if (^v_new_fr_cc[2:1]) begin
                     // Start LCD scanning
                     r_lcd_run <= 1'b1;
@@ -124,12 +124,12 @@ module z88_screen
                     r_lcd_cyc <= { r_lcd_cyc[1:0], r_lcd_cyc[2] };
                 end
             end
-            
+
             // End of line flag
             r_lcd_eol <= (r_col_ctr == 7'd107) ? 1'b1 : 1'b0;
             // End of frame flag
             r_lcd_eof <= (r_row_ctr == 6'd63) ? 1'b1 : 1'b0;
-            
+
             // Edge detect
             if (clk_ena & bus_ph) begin
                 v_new_fr_cc[2] <= v_new_fr_cc[1];
@@ -139,81 +139,25 @@ module z88_screen
             v_new_fr_cc[0] <= new_fr_tgl;
         end
     end
-    
+
     // ========================================================================
     // LCD address generator
     // ========================================================================
-    
-    reg [21:0] r_lcd_addr; // 4 MB address space
-    reg [21:9] r_pix_page; // Character pixel page
-    
-    /*
-    always @(posedge rst or posedge clk) begin : LCD_ADDR_GEN
-    
-        if (rst) begin
-            r_lcd_addr <= 22'd0;
-            r_pix_page <= 13'd0;
-        end
-        else begin
-            // Address generation
-            if (~bus_ph & r_lcd_run) begin
-                r_lcd_addr <=
-                    // Read SBA LSB
-                    {22{r_lcd_cyc[0]}} & { r_SBR[10:0], r_row_ctr[5:3], r_col_ctr[6:0], 1'b0 } |
-                    // Read SBA MSB
-                    {22{r_lcd_cyc[1]}} & { r_SBR[10:0], r_row_ctr[5:3], r_col_ctr[6:0], 1'b1 } |
-                    // Read Pixels
-                    {22{r_lcd_cyc[2]}} & { r_pix_page[21:9], r_SBA[5:0], r_row_ctr[2:0] };
-            end
-            else begin
-                r_lcd_addr <= 22'd0;
-            end
-            
-            // Address translation
-            casez ({ w_hires, r_SBA[9:6] })
-                // Lo-res RAM (7 x 64 chars)
-                5'b0?0?? : r_pix_page <= { r_PB1[9:0], r_SBA[8:6] };
-                5'b0?10? : r_pix_page <= { r_PB1[9:0], r_SBA[8:6] };
-                5'b0?110 : r_pix_page <= { r_PB1[9:0], r_SBA[8:6] };
-                // Lo-res ROM (1 x 64 chars)
-                5'b0?111 : r_pix_page <= { r_PB0[12:0]        };
-                // Hi-res ROM (3 x 256 chars)
-                5'b100?? : r_pix_page <= { r_PB2[8:0], r_SBA[9:6] };
-                5'b101?? : r_pix_page <= { r_PB2[8:0], r_SBA[9:6] };
-                5'b110?? : r_pix_page <= { r_PB2[8:0], r_SBA[9:6] };
-                // Hi-res RAM (1 x 256 chars)
-                5'b111?? : r_pix_page <= { r_PB3[10:0], r_SBA[7:6] };
-            endcase
-        end
-    end
-    */
-    
-    always @(posedge rst or posedge clk) begin : PIX_PAGE_GEN
-    
-        if (rst) begin
-            r_pix_page <= 13'd0;
-        end
-        else begin
-            // Address translation
-            casez ({ w_hires, r_SBA[9:6] })
-                // Lo-res RAM (7 x 64 chars)
-                5'b0?0?? : r_pix_page <= { r_PB1[9:0], r_SBA[8:6] };
-                5'b0?10? : r_pix_page <= { r_PB1[9:0], r_SBA[8:6] };
-                5'b0?110 : r_pix_page <= { r_PB1[9:0], r_SBA[8:6] };
-                // Lo-res ROM (1 x 64 chars)
-                5'b0?111 : r_pix_page <= { r_PB0[12:0]        };
-                // Hi-res ROM (3 x 256 chars)
-                5'b100?? : r_pix_page <= { r_PB2[8:0], r_SBA[9:6] };
-                5'b101?? : r_pix_page <= { r_PB2[8:0], r_SBA[9:6] };
-                5'b110?? : r_pix_page <= { r_PB2[8:0], r_SBA[9:6] };
-                // Hi-res RAM (1 x 256 chars)
-                5'b111?? : r_pix_page <= { r_PB3[10:0], r_SBA[7:6] };
-            endcase
-        end
-    end
-    
+
+    reg  [21:0] r_lcd_addr; // 4 MB address space
+    wire [21:9] w_pix_page; // Character pixel page
+
+    assign w_pix_page =
+    (~w_hires) ?
+      (r_SBA[8:6] == 3'b111) ?
+            {r_PB0[12:0]}                           // HRS=0; Lores0 (ROM)
+          : {r_PB1[9:0], r_SBA[8:6]}                // Lores1 (RAM)
+    : (r_SBA[9:8] == 2'b11) ?
+            {r_PB3[10:0], r_SBA[7:6]}               // HRS=1; Hires1 (RAM)
+          : {r_PB2[8:0], r_SBA[9:6]};               // Hires0 (ROM)
+
     always @(*) begin : LCD_ADDR_GEN
-    
+
         // Address generation
         if (~bus_ph & r_lcd_run) begin
             r_lcd_addr =
@@ -222,24 +166,24 @@ module z88_screen
                 // Read SBA MSB
                 {22{r_lcd_cyc[1]}} & { r_SBR[10:0], r_row_ctr[5:3], r_col_ctr[6:0], 1'b1 } |
                 // Read Pixels
-                {22{r_lcd_cyc[2]}} & { r_pix_page[21:9], r_SBA[5:0], r_row_ctr[2:0] };
+                {22{r_lcd_cyc[2]}} & { w_pix_page[21:9], r_SBA[5:0], r_row_ctr[2:0] };
         end
         else begin
             r_lcd_addr = 22'd0;
         end
     end
-    
+
     assign lcd_rden = r_lcd_run;
     assign lcd_addr = r_lcd_addr;
-    
+
     // ========================================================================
     // LCD data
     // ========================================================================
-    
+
     reg [13:0] r_SBA;       // Attributes
     reg  [7:0] r_gfx_p0;    // Pixels data
     reg        r_gfx_en_p0; // Pixels data are valid
-    
+
     wire w_hires  =  r_SBA[13];            // High resolution
     wire w_invert =  r_SBA[12];            // Inverted
     wire w_blink  =  r_SBA[11];            // 1Hz Blinking
@@ -248,9 +192,9 @@ module z88_screen
     wire w_cursor = &r_SBA[13:11];         // Cursor
     wire w_null   = (r_SBA[13:10] == 4'b1101)
                   ? 1'b1 : 1'b0;           // Null character
-    
+
     always @(posedge rst or posedge clk) begin : LCD_DATA_READ
-    
+
         if (rst) begin
             r_SBA       <= 14'b0;
             r_gfx_p0    <= 8'h00;
@@ -272,19 +216,19 @@ module z88_screen
             r_gfx_en_p0 <= lcd_vld & r_lcd_cyc[2];
         end
     end
-    
+
     // ========================================================================
     // LCD pixel pipeline
     // ========================================================================
-    
+
     reg  [8:0] r_gfx_dat_p1; // Pixels data
     reg        r_gfx_en_p1;  // Pixels data are valid
     reg  [5:0] r_gfx_row_p1; // Row number
     reg        r_gfx_eol_p1; // End of line flag
-    
+
     always @(posedge rst or posedge clk) begin : LCD_PIXEL_P1
         reg [7:0] v_gfx_p0;
-    
+
         if (rst) begin
             r_gfx_dat_p1 <= { 1'b0, 8'h00 };
             r_gfx_en_p1  <= 1'b0;
@@ -313,15 +257,15 @@ module z88_screen
             r_gfx_en_p1 <= r_gfx_en_p0;
         end
     end
-    
+
     reg  [8:0] r_gfx_dat_p2; // Pixels data shift register
     reg  [3:0] r_gfx_en_p2;  // Pixels data are valid
     reg  [5:0] r_gfx_row_p2; // Row number
     reg        r_gfx_eol_p2; // End of line flag
     reg  [8:0] r_gfx_ctr_p2; // Pixel counter
-    
+
     always @(posedge rst or posedge clk) begin : LCD_PIXEL_P2
-    
+
         if (rst) begin
             r_gfx_dat_p2 <= { 1'b0, 8'b00_00_00_00 };
             r_gfx_en_p2  <= 4'b0000;
@@ -366,9 +310,9 @@ module z88_screen
             end
         end
     end
-    
+
     assign vram_we   = r_gfx_en_p2[3];
     assign vram_data = r_gfx_dat_p2[8:6];
     assign vram_addr = { r_gfx_ctr_p2, r_gfx_row_p2 };
-    
+
 endmodule
